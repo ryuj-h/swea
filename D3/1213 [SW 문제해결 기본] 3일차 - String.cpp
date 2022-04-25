05c7e3bc098ec85b006b91d05ea5e171dfc58938
Ryu-J <37369443+ryuj-h@users.noreply.github.com>
2022-04-25 23:13:36 +0900

    Create 1213 [SW 문제해결 기본] 3일차 - String.cpp

---
+++ D3/1213 [SW 문제해결 기본] 3일차 - String.cpp
@@ -0,0 +1,73 @@
+/*
+
+소요시간 : 13:50
+
+
+
+
+주어지는 영어 문장에서 특정한 문자열의 개수를 반환하는 프로그램을 작성하여라.
+
+Starteatingwellwiththeseeighttipsforhealthyeating,whichcoverthebasicsofahealthydietandgoodnutrition.
+
+위 문장에서 ti 를 검색하면, 답은 4이다.
+
+[제약 사항]
+
+총 10개의 테스트 케이스가 주어진다.
+
+문장의 길이는 1000자를 넘어가지 않는다.
+
+한 문장에서 검색하는 문자열의 길이는 최대 10을 넘지 않는다.
+
+한 문장에서는 하나의 문자열만 검색한다. 
+
+[입력]
+
+각 테스트 케이스의 첫 줄에는 테스트 케이스의 번호가 주어지고 그 다음 줄에는 찾을 문자열, 그 다음 줄에는 검색할 문장이 주어진다.
+
+[출력]
+
+#부호와 함께 테스트 케이스의 번호를 출력하고, 공백 문자 후 테스트 케이스의 답을 출력한다.
+
+
+
+
+
+*/
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	for (test_case = 1; test_case <= 10; ++test_case)
+	{
+		int Sol = 0;
+		
+		std::string LongStr;
+		std::string Tofind;
+
+		cin >> T;
+		cin >> Tofind;
+		cin >> LongStr;
+
+		int lastpos = 0;
+		while (true)
+		{
+			auto n = LongStr.find(Tofind, lastpos);
+			if (n == std::string::npos)
+				break;
+			lastpos = n + 1;
+			Sol++;
+		}
+		
+		
+
+		cout << "#" << test_case << " " << Sol << endl;
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
